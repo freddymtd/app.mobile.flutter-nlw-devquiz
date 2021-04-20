405f47ff17576d6a730078da8e15b2962fc88386
freddymtd <freddy-zenteno@hotmail.com>
2021-04-20 15:32:37 -0300

    Base inicial do projeto com img, fonts e plugins

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'core/app_widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AppWidget(),
+    );
+  }
+}
