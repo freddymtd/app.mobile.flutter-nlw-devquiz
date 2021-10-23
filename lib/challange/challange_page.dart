12bad62147fed526dad53efcbc0a7570792aa4c4
freddymtd <freddy-zenteno@hotmail.com>
2021-10-23 17:19:33 -0300

    Gerenciador de estado no quiz e implementação de navegação entre questões do quiz

--- lib/challange/challange_page.dart
+++ lib/challange/challange_page.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/challange/challange_controller.dart';
 import 'package:devquiz/challange/widgets/next_button/next_button_widget.dart';
 import 'package:devquiz/challange/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:devquiz/challange/widgets/quiz/quiz_widget.dart';
@@ -15,6 +16,18 @@ class ChallangePage extends StatefulWidget {
 }
 
 class _ChallangePageState extends State<ChallangePage> {
+  final controller = ChallangeController();
+  final pageController = PageController();
+
+  @override
+  void initState() {
+    pageController.addListener(() {
+      controller.currentPage = pageController.page!.toInt() + 1;
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,13 +39,21 @@ class _ChallangePageState extends State<ChallangePage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               BackButton(),
-              QuestionIndicatorWidget(),
+              ValueListenableBuilder<int>(
+                valueListenable: controller.currentPageNotifier,
+                builder: (context, value, _) => QuestionIndicatorWidget(
+                  currentPage: value,
+                  length: widget.questions.length,
+                ),
+              ),
             ],
           ),
         ),
       ),
-      body: QuizWidget(
-        question: widget.questions[0],
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: widget.questions.map((e) => QuizWidget(question: e)).toList(),
       ),
       bottomNavigationBar: SafeArea(
         bottom: true,
@@ -43,8 +64,12 @@ class _ChallangePageState extends State<ChallangePage> {
             children: [
               Expanded(
                   child: NextButtonWidget.white(
-                label: "Fácil",
-                onTap: () {},
+                label: "Pular",
+                onTap: () {
+                  pageController.nextPage(
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.linear);
+                },
               )),
               SizedBox(width: 8),
               Expanded(
